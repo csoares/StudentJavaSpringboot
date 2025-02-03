5c280c71425180d7abd3424e7366df7fa1932c3a
Chris <soareschristophe@gmail.com>
2025-02-03 20:54:41 +0100

    last commit

--- src/main/java/mindera/springboot/studentsapi/aspects/DivisionExceptionHandler.java
+++ src/main/java/mindera/springboot/studentsapi/aspects/DivisionExceptionHandler.java
@@ -3,6 +3,8 @@ package mindera.springboot.studentsapi.aspects;
 import mindera.springboot.studentsapi.exceptions.BadDenominatorDivision;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.NonTransientDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -15,7 +17,20 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 public class DivisionExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(DivisionExceptionHandler.class);
 
-    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class, NumberFormatException.class, ArithmeticException.class, BadDenominatorDivision.class})
+    @ExceptionHandler(value = { DataIntegrityViolationException.class})
+    public ResponseEntity<String> sqlError(Exception ex) {
+        logger.error("Known Exception: " + ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("unexpeted error about sql");
+    }
+
+
+    @ExceptionHandler(value = { BadDenominatorDivision.class})
+    public ResponseEntity<String> baddivide(Exception ex) {
+        logger.error("Known Exception: " + ex);
+        return ResponseEntity.status(200).body("1");
+    }
+
+    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class,  NumberFormatException.class, ArithmeticException.class})
     public ResponseEntity<String> handleDivisionByZero(Exception ex) {
         logger.error("Known Exception: " + ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
@@ -26,4 +41,8 @@ public class DivisionExceptionHandler {
         logger.error("Unknown Exception: " + ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.");
     }
+
+
+
+
 }
